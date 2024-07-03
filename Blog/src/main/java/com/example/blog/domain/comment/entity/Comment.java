5f7ad99a01b53967c3c5c066b444d535f9266dde
parkseokwon <parkswon1@naver.com>
2024-07-03 14:23:24 +0900

    ✨ feat Comment.java
    Comment 생성

--- Blog/src/main/java/com/example/blog/domain/comment/entity/Comment.java
+++ Blog/src/main/java/com/example/blog/domain/comment/entity/Comment.java
@@ -1,4 +1,35 @@
 package com.example.blog.domain.comment.entity;
 
+import com.example.blog.domain.post.entity.Post;
+import com.example.blog.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
 }
