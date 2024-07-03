bffb25e1118d4b304fc02f093e919418dd3e1ec5
parkseokwon <parkswon1@naver.com>
2024-07-03 14:24:30 +0900

    ✨ feat Tag.java
    Tag entitiy 생성

--- Blog/src/main/java/com/example/blog/domain/tag/entity/Tag.java
+++ Blog/src/main/java/com/example/blog/domain/tag/entity/Tag.java
@@ -1,4 +1,25 @@
 package com.example.blog.domain.tag.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
 }
