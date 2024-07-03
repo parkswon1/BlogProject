9204aee436b75656000b43494e2d5497c128b10b
parkseokwon <parkswon1@naver.com>
2024-07-03 14:22:58 +0900

    ✨ feat Blog.java
    entitiy 생성

--- Blog/src/main/java/com/example/blog/domain/blog/entity/Blog.java
+++ Blog/src/main/java/com/example/blog/domain/blog/entity/Blog.java
@@ -1,4 +1,41 @@
 package com.example.blog.domain.blog.entity;
 
+import com.example.blog.domain.image.entity.Image;
+import com.example.blog.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int views;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
 }
