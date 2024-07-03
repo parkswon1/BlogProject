ee626996512212b78b9f9509f8477c486644bda4
parkseokwon <parkswon1@naver.com>
2024-07-03 14:24:41 +0900

    ✨ feat User.java
    User entitiy 생성

--- Blog/src/main/java/com/example/blog/domain/user/entity/User.java
+++ Blog/src/main/java/com/example/blog/domain/user/entity/User.java
@@ -1,4 +1,36 @@
 package com.example.blog.domain.user.entity;
 
+import com.example.blog.domain.image.entity.Image;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Image image;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
 }
