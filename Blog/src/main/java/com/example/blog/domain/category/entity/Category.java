04d7bfc2345801bfa38b1f640515c93338cd3177
parkseokwon <parkswon1@naver.com>
2024-07-03 14:23:14 +0900

    ✨ feat Category.java
    Category 생성

--- Blog/src/main/java/com/example/blog/domain/category/entity/Category.java
+++ Blog/src/main/java/com/example/blog/domain/category/entity/Category.java
@@ -1,4 +1,25 @@
 package com.example.blog.domain.category.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
 }
