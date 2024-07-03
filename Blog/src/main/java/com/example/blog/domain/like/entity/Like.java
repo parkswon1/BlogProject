08499e9942d0332aa287dabf2bd408991dbedff9
parkseokwon <parkswon1@naver.com>
2024-07-03 14:24:04 +0900

    ✨ feat Like.java
    Like entitiy 생성

--- Blog/src/main/java/com/example/blog/domain/like/entity/Like.java
+++ Blog/src/main/java/com/example/blog/domain/like/entity/Like.java
@@ -1,4 +1,39 @@
 package com.example.blog.domain.like.entity;
 
+import com.example.blog.domain.blog.entity.Blog;
+import com.example.blog.domain.comment.entity.Comment;
+import com.example.blog.domain.post.entity.Post;
+import com.example.blog.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
 public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id")
+    private Blog blog;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
 }
