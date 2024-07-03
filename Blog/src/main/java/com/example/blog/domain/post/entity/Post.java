12aedfaa8c416b89c2727af1a1e25551dfeea5a0
parkseokwon <parkswon1@naver.com>
2024-07-03 14:24:15 +0900

    ✨ feat Post.java
    Post entitiy 생성

--- Blog/src/main/java/com/example/blog/domain/post/entity/Post.java
+++ Blog/src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -1,4 +1,46 @@
 package com.example.blog.domain.post.entity;
 
+import com.example.blog.domain.blog.entity.Blog;
+import com.example.blog.domain.category.entity.Category;
+import com.example.blog.domain.image.entity.Image;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id", nullable = false)
+    private Blog blog;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @OneToOne
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int views;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
 }
