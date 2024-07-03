f9037551312487d6229b15abed0d8aed71cbca04
parkseokwon <parkswon1@naver.com>
2024-07-03 14:23:47 +0900

    ✨ feat Image.java
    Image 생성

--- Blog/src/main/java/com/example/blog/domain/image/entity/Image.java
+++ Blog/src/main/java/com/example/blog/domain/image/entity/Image.java
@@ -1,4 +1,18 @@
 package com.example.blog.domain.image.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
 }
