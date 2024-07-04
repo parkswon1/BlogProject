e94a7d3ed1e11791d76a14c6fece2f862faf9d1b
parkseokwon <parkswon1@naver.com>
2024-07-05 00:38:41 +0900

    ✨ feat CustomException.java
    여러가지의 오류 메세지를 보낼 예외 클래스

---
+++ Blog/src/main/java/com/example/blog/common/exception/auth/CustomException.java
@@ -0,0 +1,7 @@
+package com.example.blog.common.exception.auth;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
