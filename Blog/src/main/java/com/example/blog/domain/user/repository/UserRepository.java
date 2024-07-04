f495b9d582aff008527aa133eb08a1386ce3deb2
parkseokwon <parkswon1@naver.com>
2024-07-05 00:50:58 +0900

    ♻️ refactor UserRepository.java
    사용자 id값을 username으로 만드는 로직 추가
    username으로 User의 Object를 반환해주는 로직 추가

--- Blog/src/main/java/com/example/blog/domain/user/repository/UserRepository.java
+++ Blog/src/main/java/com/example/blog/domain/user/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.example.blog.domain.user.repository;
 
-public class UserRepository {
+import com.example.blog.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    @Query("SELECT u.id FROM User u WHERE u.username = :username")
+    Long findIdByUsername(@Param("username") String username);
 }
