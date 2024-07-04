70f6ee4eab1a43bc352bcbf1d6bb4c38996f8184
parkseokwon <parkswon1@naver.com>
2024-07-05 00:29:01 +0900

    ♻️ feat start.sql
    email 칼럼을 안쓰고 username을 email로 사용하기로함

--- Blog/src/main/resources/sql/start.sql
+++ Blog/src/main/resources/sql/start.sql
@@ -7,7 +7,6 @@ use BlogDB;
 CREATE TABLE `User` (
                         `id` int PRIMARY KEY AUTO_INCREMENT,
                         `username` varchar(255) UNIQUE NOT NULL,
-                        `email` varchar(255) UNIQUE NOT NULL,
                         `password` varchar(255) NOT NULL,
                         `image_id` int,
                         `created_at` datetime DEFAULT (current_timestamp),
