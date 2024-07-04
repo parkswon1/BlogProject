70d7f024e17ffabcf1f2374e941622cb8b1c3959
parkseokwon <parkswon1@naver.com>
2024-07-05 00:43:42 +0900

    ✨ feat GlobalExceptionHandler.java
    전역 예외 처리들을 모아두는 핸들러

---
+++ Blog/src/main/java/com/example/blog/common/exception/auth/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.blog.common.exception.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessTokenExpiredException.class)
+    public ResponseEntity<Object> handleAccessTokenExpiredException(AccessTokenExpiredException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ex.getMessage());
+    }
+}
