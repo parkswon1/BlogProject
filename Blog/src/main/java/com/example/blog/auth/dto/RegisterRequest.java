fbe25913c2e32b8653c393426b0da4e6db79119e
parkseokwon <parkswon1@naver.com>
2024-07-06 15:48:40 +0900

    ♻️	refactor RegisterRequest.java
    username을 이메일 형식으로 받아야 한다와 비밀번호가 있어야 한다를 추가

--- Blog/src/main/java/com/example/blog/auth/dto/RegisterRequest.java
+++ Blog/src/main/java/com/example/blog/auth/dto/RegisterRequest.java
@@ -2,10 +2,16 @@ package com.example.blog.auth.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Setter
 @Getter
 public class RegisterRequest {
+    @NotBlank(message = "username이 필요합니다.")
+    @Email(message = "username은 이메일 형식이여야 합니다.")
     private String username;
+
+    @NotBlank(message = "비밀번호가 있어야 합니다.")
     private String password;
 }
