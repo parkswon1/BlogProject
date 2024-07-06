e55699aa0613bcfa9b373787abcdd469062ba306
parkseokwon <parkswon1@naver.com>
2024-07-06 15:48:30 +0900

    ♻️	refactor LoginRequest.java
    username을 이메일 형식으로 받아야 한다와 비밀번호가 있어야 한다를 추가

--- Blog/src/main/java/com/example/blog/auth/dto/LoginRequest.java
+++ Blog/src/main/java/com/example/blog/auth/dto/LoginRequest.java
@@ -2,11 +2,17 @@ package com.example.blog.auth.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 //로그인 할때 필요한 dto
 @Setter
 @Getter
 public class LoginRequest {
+    @NotBlank(message = "username이 필요합니다.")
+    @Email(message = "username은 이메일 형식이여야 합니다.")
     private String username;
+
+    @NotBlank(message = "비밀번호가 있어야 합니다.")
     private String password;
 }
