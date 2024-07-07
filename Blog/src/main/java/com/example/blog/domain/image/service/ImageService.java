e9fe33afc7bbfc5947b61e1f0e02e6e7a698fded
parkseokwon <parkswon1@naver.com>
2024-07-07 16:14:23 +0900

    ✨	feat ImageService.java
    나중에 S3를 연결할 것을 예상해서 interface생성
    
    local과
    S3를 둘다 만들예정

--- Blog/src/main/java/com/example/blog/domain/image/service/ImageService.java
+++ Blog/src/main/java/com/example/blog/domain/image/service/ImageService.java
@@ -1,4 +1,12 @@
 package com.example.blog.domain.image.service;
 
-public class ImageService {
+import com.example.blog.domain.image.entity.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ImageService {
+    Image saveImage(MultipartFile file) throws IOException;
+    Image findById(Long id);
+    String getImageUrl(Image image, boolean isThumbnail);
 }
