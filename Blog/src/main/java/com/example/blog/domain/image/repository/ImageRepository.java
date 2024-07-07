b5e810a65765c736fc569d7fe8e815ad48712425
parkseokwon <parkswon1@naver.com>
2024-07-07 16:02:06 +0900

    ✨	feat ImageRepository.java
    db에서 이미지 url받아올 리파지 토리

--- Blog/src/main/java/com/example/blog/domain/image/repository/ImageRepository.java
+++ Blog/src/main/java/com/example/blog/domain/image/repository/ImageRepository.java
@@ -1,4 +1,10 @@
 package com.example.blog.domain.image.repository;
 
-public class ImageRepository {
+
+import com.example.blog.domain.image.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
 }
