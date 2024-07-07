acc60027ff6df25ec0adeb9aed7ef63b535859db
parkseokwon <parkswon1@naver.com>
2024-07-08 00:10:35 +0900

    ✨	feat UserService.java
    인터페이스 설계
    
    1. 사용자 정보 업데이트 (위에서 만든 image이벤트 사용해야함, 비밀번호는 업데이트 안함)
    2. 사용자 정보 불러오기 (위에서 만든 image이벤트 사용해야함, 비밀번호는 줄필요 없음)
    3. 사용자 프로필 불러오기 (위에서 만든 image이벤트 사용해야함)
    4. 사용자 프로필 저장하기 (위에서 만든 image이벤트 사용해야함)
    5. 사용자 비밀번호 변경하기

--- Blog/src/main/java/com/example/blog/domain/user/service/UserService.java
+++ Blog/src/main/java/com/example/blog/domain/user/service/UserService.java
@@ -1,4 +1,12 @@
 package com.example.blog.domain.user.service;
 
-public class UserService {
+import com.example.blog.domain.user.entity.User;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface UserService {
+    User updateUser(Long userId, String name);
+    User getUesr(Long userId);
+    User getUserProfile(Long userId);
+    User saveUserProfile(Long userId, MultipartFile imageFile);
+    void changePassword(Long userId, String newPassword);
 }
