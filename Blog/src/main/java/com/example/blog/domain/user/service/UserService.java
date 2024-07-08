21ea2e27529db07ac57cac9e36f093a373df64dc
parkseokwon <parkswon1@naver.com>
2024-07-08 16:27:30 +0900

    ♻️	refactor getUserProfile.java
    Image를 넘겨줘서 객체지향적으로 가도록 변경

--- Blog/src/main/java/com/example/blog/domain/user/service/UserService.java
+++ Blog/src/main/java/com/example/blog/domain/user/service/UserService.java
@@ -1,12 +1,13 @@
 package com.example.blog.domain.user.service;
 
+import com.example.blog.domain.image.entity.Image;
 import com.example.blog.domain.user.entity.User;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
     void updateUser(Long userId, String name) throws IllegalAccessException;
     User getUser(Long userId) throws IllegalAccessException;
-    String getUserProfile(Long userId) throws IllegalAccessException;
+    Image getUserProfile(Long userId) throws IllegalAccessException;
     void saveUserProfile(Long userId, MultipartFile imageFile) throws IllegalAccessException;
     void changePassword(Long userId, String newPassword, String checkPassword) throws IllegalAccessException;
     User getUserWithOutPassword(Long userId) throws IllegalAccessException;
