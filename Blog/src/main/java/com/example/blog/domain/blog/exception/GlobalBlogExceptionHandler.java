8e30133bf61d7b7413287381c219fc7e793be6ee
parkseokwon <parkswon1@naver.com>
2024-07-10 00:32:52 +0900

    ✨	feat GlobalBlogExceptionHandler.java
    블로그 예외 클래스 생성

---
+++ Blog/src/main/java/com/example/blog/domain/blog/exception/GlobalBlogExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.example.blog.domain.blog.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalBlogExceptionHandler {
+}
