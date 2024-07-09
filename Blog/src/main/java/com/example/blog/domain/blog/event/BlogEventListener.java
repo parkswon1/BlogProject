d6fea6a85eea89a92429bdf33ebda502c1c3d473
parkseokwon <parkswon1@naver.com>
2024-07-10 00:30:50 +0900

    ✨	feat BlogEventListener.java
    블로그를 볼시 블로그 주인이 보는거 아니면 블로그 view증가하는 이벤트

--- Blog/src/main/java/com/example/blog/domain/blog/event/BlogEventListener.java
+++ Blog/src/main/java/com/example/blog/domain/blog/event/BlogEventListener.java
@@ -1,4 +1,21 @@
 package com.example.blog.domain.blog.event;
 
+import com.example.blog.domain.blog.repository.BlogRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class BlogEventListener {
+    private final BlogRepository blogRepository;
+
+    @EventListener
+    public void handleBlogViewEvent(BlogViewEvent event){
+        if (!event.getBlog().getUser().getId().equals(event.getUserId())) {
+            event.getBlog().setViews(event.getBlog().getViews() + 1);
+            blogRepository.save(event.getBlog());
+        }
+    }
 }
