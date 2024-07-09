e304816c493afd6b9e39725c260e536aa12b4666
parkseokwon <parkswon1@naver.com>
2024-07-10 00:31:57 +0900

    ✨	feat BlogService.java
    블로그 서비스 인터페이스

--- Blog/src/main/java/com/example/blog/domain/blog/service/BlogService.java
+++ Blog/src/main/java/com/example/blog/domain/blog/service/BlogService.java
@@ -1,4 +1,14 @@
 package com.example.blog.domain.blog.service;
 
-public class BlogService {
+
+import com.example.blog.domain.blog.entity.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface BlogService {
+    void saveBlog(Long userid, String title, String text, MultipartFile imageFile);
+    Blog getBlogByBlogId(Long blogId, Long viewerId) throws IllegalAccessException;
+    Blog getBlogByUserId(Long uesrId, Long viewerId) throws IllegalAccessException;
+    Page<Blog> getBlogsList(Pageable pageable) throws IllegalAccessException;
 }
