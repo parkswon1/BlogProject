ff593dfdc2764c88d22f90d715c9f5203dc3cc16
parkseokwon <parkswon1@naver.com>
2024-07-10 00:31:11 +0900

    ✨	feat BlogRepository.java
    블로그 리파지토리 생성

--- Blog/src/main/java/com/example/blog/domain/blog/repository/BlogRepository.java
+++ Blog/src/main/java/com/example/blog/domain/blog/repository/BlogRepository.java
@@ -1,4 +1,12 @@
 package com.example.blog.domain.blog.repository;
 
-public class BlogRepository {
+import com.example.blog.domain.blog.entity.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+    Optional<Blog> findByUserId(Long userId);
 }
