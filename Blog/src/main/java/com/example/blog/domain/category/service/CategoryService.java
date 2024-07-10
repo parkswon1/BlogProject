678c411afc495f8cf4791387b8f60ed6434ae15f
parkseokwon <parkswon1@naver.com>
2024-07-10 23:41:37 +0900

    ✨	feat CategoryService.java
    카테고리 서비스 인터페이스 생성

--- Blog/src/main/java/com/example/blog/domain/category/service/CategoryService.java
+++ Blog/src/main/java/com/example/blog/domain/category/service/CategoryService.java
@@ -1,4 +1,13 @@
 package com.example.blog.domain.category.service;
 
-public class CategoryService {
+import com.example.blog.domain.category.entity.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    Category createCategory(String name, Long parentId) throws IllegalAccessException;
+    Category getCategoryById(Long id);
+    List<Category> getAllCategories();
+    List<Category> getSubCategories(Long parentId);
+    void deleteCategory(Long id) throws IllegalAccessException;
 }
