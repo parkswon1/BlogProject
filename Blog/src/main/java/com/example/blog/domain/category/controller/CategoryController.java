ffa62b0878a98f3c327ae0239e952cf26c33d7ac
parkseokwon <parkswon1@naver.com>
2024-07-10 23:40:27 +0900

    ✨	feat CategoryController.java
    카테고리 관련 컨트롤러
    1. 카테고리 생성
    2. 카테고리 가져오기
    3. 모든 카테고리 가져오기
    4. 자식 카테고리 가져오기
    5. 카테고리 삭제하기

--- Blog/src/main/java/com/example/blog/domain/category/controller/CategoryController.java
+++ Blog/src/main/java/com/example/blog/domain/category/controller/CategoryController.java
@@ -1,4 +1,60 @@
 package com.example.blog.domain.category.controller;
 
+import com.example.blog.domain.category.entity.Category;
+import com.example.blog.domain.category.service.CategoryService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<?> createCategory(@RequestParam String name,
+                                                   @RequestParam(required = false) Long parentId){
+        try {
+            Category category = categoryService.createCategory(name, parentId);
+            return ResponseEntity.ok(category);
+        } catch (IllegalAccessException e) {
+            return ResponseEntity.status(404).body(e);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCategoryById(@PathVariable Long id){
+        try {
+            Category category = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(category);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(404).body(e);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories(){
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{parentId}/subcategories")
+    public ResponseEntity<List<Category>> getSubCategories(@PathVariable Long parentId){
+        List<Category> subCategories = categoryService.getSubCategories(parentId);
+        return ResponseEntity.ok(subCategories);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletCategory(@PathVariable Long id){
+        try {
+            categoryService.deleteCategory(id);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalAccessException e) {
+            return ResponseEntity.badRequest().body(e);
+        }
+    }
 }
