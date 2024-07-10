40d853ec81487a24f9cf1d45a1fa134e1f4acf6d
parkseokwon <parkswon1@naver.com>
2024-07-10 23:37:54 +0900

    ✨	feat BlogEventListener.java
    handleBlogFetchEvent 메서드 생성
    블로그 아이디를 입력하면 Blog객체를 돌려주는 이벤트 메서드

--- Blog/src/main/java/com/example/blog/domain/blog/event/BlogEventListener.java
+++ Blog/src/main/java/com/example/blog/domain/blog/event/BlogEventListener.java
@@ -1,5 +1,6 @@
 package com.example.blog.domain.blog.event;
 
+import com.example.blog.domain.blog.entity.Blog;
 import com.example.blog.domain.blog.repository.BlogRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,5 +18,12 @@ public class BlogEventListener {
             event.getBlog().setViews(event.getBlog().getViews() + 1);
             blogRepository.save(event.getBlog());
         }
+    }
+
+    @EventListener
+    public void handleBlogFetchEvent(BlogFetchEvent event){
+        Blog blog = blogRepository.findById(event.getBlogId())
+                .orElseThrow(() -> new IllegalArgumentException("블로그를 찾을 수 없습니다. : " + event.getBlogId()));
+        event.setBlog(blog);
     }
 }
