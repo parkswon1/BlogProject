a03e4df3664b5ea55219660fdca3e45b666eed60
parkseokwon <parkswon1@naver.com>
2024-07-10 23:41:17 +0900

    ✨	feat CategoryRepository.java
    카테고리 리파지톡리 생성

--- Blog/src/main/java/com/example/blog/domain/category/repository/CategoryRepository.java
+++ Blog/src/main/java/com/example/blog/domain/category/repository/CategoryRepository.java
@@ -1,4 +1,12 @@
 package com.example.blog.domain.category.repository;
 
-public class CategoryRepository {
+import com.example.blog.domain.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+    List<Category> findByParent(Category parent);
 }
