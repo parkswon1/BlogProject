4bcdec1bca6d9ad63721e9f1017d7137c0350057
parkseokwon <parkswon1@naver.com>
2024-07-10 23:45:08 +0900

    ✨	feat PostEventListener.java
    게시글을 id로 받아서 조회수를 올리는 메서드

--- Blog/src/main/java/com/example/blog/domain/post/event/PostEventListener.java
+++ Blog/src/main/java/com/example/blog/domain/post/event/PostEventListener.java
@@ -1,4 +1,21 @@
 package com.example.blog.domain.post.event;
 
+import com.example.blog.domain.post.entity.Post;
+import com.example.blog.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class PostEventListener {
+    private final PostRepository postRepository;
+
+    @EventListener
+    public void handlePostViewIncrementEvent(PostViewIncrementEvent event){
+        Post post = postRepository.findById(event.getPostId())
+                .orElseThrow(() -> new IllegalArgumentException("Post not found with id: " + event.getPostId()));
+        post.setViews(post.getViews() + 1);
+        postRepository.save(post);
+    }
 }
