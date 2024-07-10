894c7c6dafb9a33dc88eda7558ef1c84b4e3884f
parkseokwon <parkswon1@naver.com>
2024-07-10 23:40:49 +0900

    ✨	feat CategoryEventListener.java
    카테고리 이벤트 카테고리 id를 입력하면 카테고리 객체를 보내줌

--- Blog/src/main/java/com/example/blog/domain/category/event/CategoryEventListener.java
+++ Blog/src/main/java/com/example/blog/domain/category/event/CategoryEventListener.java
@@ -1,4 +1,20 @@
 package com.example.blog.domain.category.event;
 
+import com.example.blog.domain.category.entity.Category;
+import com.example.blog.domain.category.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class CategoryEventListener {
+    private final CategoryRepository categoryRepository;
+
+    @EventListener
+    public void handleCategoryFetchEvent(CategoryFetchEvent event){
+        Category category = categoryRepository.findById(event.getCategoryId())
+                .orElseThrow(() -> new IllegalArgumentException("카테고리가 없습니다." + event.getCategoryId()));
+        event.setCategory(category);
+    }
 }
