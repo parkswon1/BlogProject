5feebb747ea716a9881eb987d3bb780cf0284964
parkseokwon <parkswon1@naver.com>
2024-07-10 23:45:23 +0900

    ✨	feat PostRepository.java
    게시글 리파지토리 생성

--- Blog/src/main/java/com/example/blog/domain/post/repository/PostRepository.java
+++ Blog/src/main/java/com/example/blog/domain/post/repository/PostRepository.java
@@ -1,4 +1,10 @@
 package com.example.blog.domain.post.repository;
 
-public class PostRepository {
+import com.example.blog.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByBlogId(Long blogId, Pageable pageable);
 }
